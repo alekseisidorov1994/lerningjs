5c26b752bc696f2efb6087fbd00365a6781d79b8
alex <zxc080294@gmail.com>
2019-09-07 20:16:28 +0700

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+let obj = {
+    name: "John",
+    age: 25,
+    isMarried: false,
+    haveCar: true
+};
+let arr = ['plum.png','orang.jpg', 'apple.bmp'];
+console.log(arr);
+// let answer = prompt("are you ready?", " ");
+
+let incr = 10;
+let decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+console.log("2" === 2);
